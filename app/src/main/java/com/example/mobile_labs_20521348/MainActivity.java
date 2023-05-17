51b7de56dfa39319e5b4d25446ca969cc0143072
Hoang - UIT <nhoang200260@gmail.com>
2023-05-17 14:16:36 +0700

    first commit

---
+++ app/src/main/java/com/example/mobile_labs_20521348/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile_labs_20521348;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
